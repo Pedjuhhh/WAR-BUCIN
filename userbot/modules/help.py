cb11d0145c2be62a65b8f182f37ae9fb9e296d50
Mpeyy <85046635+Mpey0503@users.noreply.github.com>
2021-06-07 16:43:37 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,7 +33,7 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t ✯  "
-        await event.edit("**☯ 𝑃𝐸𝑌𝑌-𝑈𝑆𝐸𝑅𝐵𝑂𝑇 ☯⭀**\n\n"
+        await event.edit("**❃ 𝑃𝐸𝑌𝑌-𝑈𝑆𝐸𝑅𝐵𝑂𝑇 ❃**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
                          f"◉ {string}◉\n\n")

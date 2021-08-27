227555a32d7fbfab8f1cd34275654d8739db21a6
BabyBeelzebub <89534912+BabyBeelzebub@users.noreply.github.com>
2021-08-27 22:56:29 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⭕PEYY - PROJECTS⭕  🛠️ V{BOT_VER} [TELAH AKTIF TOLOL]")
+    f"⭕•ʙɛɛ⸸ʑɛʙᴜʙ✰̶✰̶⭕  🛠️ V{BOT_VER} [ AKTIF ]")
 
 
 if len(argv) not in (1, 3, 4):

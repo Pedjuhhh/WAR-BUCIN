68f7163ad573bfc017f59274abf95c7d1ea69d12
Pedjuhhh <89705396+Pedjuhhh@users.noreply.github.com>
2021-08-29 02:30:11 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -106,7 +106,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/BabyBeelzebub/WAR-BUCIN")
+    "https://github.com/Pedjuhhh/WAR-BUCIN")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Geez-UserBot")
 
